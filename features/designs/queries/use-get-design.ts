0df75c2cd11d27b3d11d10e5a5c520f0cb2ce779
baoha7204 <baoha3604@gmail.com>
2025-06-29 19:25:32 +0700

    hotfix: add private Axios for authenticated routes

--- features/designs/queries/use-get-design.ts
+++ features/designs/queries/use-get-design.ts
@@ -1,11 +1,12 @@
-import { customAxios } from "@/config/axios";
+import usePrivateAxios from "@/hooks/usePrivateAxios";
 import { useQuery } from "@tanstack/react-query";
 
 export function useGetDesign() {
+  const axiosPrivate = usePrivateAxios();
   return useQuery({
     queryKey: ["design"],
     queryFn: async () => {
-      const response = await customAxios.get("/design");
+      const response = await axiosPrivate.get("/design");
       if (response.status !== 200) {
         throw new Error("Failed to fetch design");
       }
